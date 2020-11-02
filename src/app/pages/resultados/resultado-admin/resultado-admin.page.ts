fce70e2ffad550d6cba584bee3b3c8c1b236c9ff
Carlos Romero <soportecarlosromero@gmail.com>
2020-11-02 14:27:11 -0500

    Instala  chart.js y hace ejemplo burn basico

--- src/app/pages/resultados/resultado-admin/resultado-admin.page.ts
+++ src/app/pages/resultados/resultado-admin/resultado-admin.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-resultado-admin',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./resultado-admin.page.scss'],
 })
 export class ResultadoAdminPage implements OnInit {
-
-  constructor() { }
+  chart: any;
+  constructor(private _emp: DataService) { }
 
   ngOnInit() {
+    this.chart = new Chart('canvas', {
+      type: 'doughnut',
+      data: {
+        labels: ['Aciertos', 'Desaciertos'],
+        datasets: [
+          {
+            data: [71, 3],
+            backgroundColor: ['rgba(45, 211, 111, 1)', 'rgba(235, 68, 90, 0.5)'],
+            fill: false
+          },
+        ]
+      },
+      options: {
+        legend: {
+          display: true
+        },
+        tooltips:{
+          enabled:false
+        }
+      }
+    });
   }
 
 }
